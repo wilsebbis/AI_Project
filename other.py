484eaee064997093f911a50e7e96bf57e65a542d
Wil Bishop <wilsebbis@wilsebbis.com>
2025-05-05 14:33:44 -0500

    Deleted unnecessary testing files

--- other.py
+++
@@ -1,5 +0,0 @@
-import bcrypt
-
-master_password = "your_master_password"  # Replace with your actual master password
-hashed_password = bcrypt.hashpw(master_password.encode(), bcrypt.gensalt())
-print(hashed_password.decode())
